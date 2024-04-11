a70ecfd3fe676d112e4c925859222527f43742a9
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-04-11 18:08:06 +0300

    update components

--- lottery/src/Components/Lottery.js
+++ lottery/src/Components/Lottery.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import LotteryTicket from "./LotteryTicket";
 
 class Lottery extends Component {
 
   renderButton() {
-    const {remaningTickets, actions} = this.props;
+    const { remaningTickets, actions } = this.props;
 
     if (remaningTickets > 0) {
       return <button onClick={actions.registerTicket}>КУПИ БИЛЕТ</button>
     }
+  }
+
+  renderTickets() {
+    const { tickets, actions } = this.props;
+    const lotteryTicketAction = { removeTicket: actions.removeTicket }
+
+    const lotteryTickets = tickets.map((ticket, index) => {
+      return (
+        <LotteryTicket actions={lotteryTicketAction} color={ticket.color} number={ticket.number} index={index} key={index} />
+      );
+    });
+
+    return lotteryTickets;
   }
 
   render() {
@@ -17,6 +31,9 @@ class Lottery extends Component {
         {this.renderButton()}
         <br />
         <small>оставащи: {this.props.remaningTickets}</small>
+        <br />
+        <hr />
+        {this.renderTickets()}
       </>
     );
   }
--- lottery/src/Components/LotteryTicket.js
+++ lottery/src/Components/LotteryTicket.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 
-class LotteryTicket extends Component{
-  render(){
-    return;
+class LotteryTicket extends Component {
+  render() {
+    const { actions, index, color, number } = this.props;
+
+    return (
+      <div style={{ backgroundColor: color, padding: 5, width: "60%", margin: "auto" }}
+        onClick={() => { actions.removeTicket(index) }}>
+        <button style={{ float: "left" }}>X</button>
+        <small>Това билетче има числото: <b>{number}</b></small>
+      </div>
+    );
   }
 }
 
